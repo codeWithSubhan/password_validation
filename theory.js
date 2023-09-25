fa5a85de79f776f63e4bb67eb60dba9b44657ff4
subhan <suvhan13@gmail.com>
2023-09-25 23:03:08 +0530

    new file added

--- theory.js
+++ theory.js
@@ -2,8 +2,9 @@
 // use to create a new git repo means first make project on pc then upload on git repo
 
 // git init
-// git remote add origin url
+// git remote add origin url   (remote means add new repository)
 // git remote -v to verify remote
 // git branch  to check branch
 // git branch -m main  to rename branch
 // git push origin main
+// git push -u origin main  (-u means set-upstreams means dont rewite origin main)
