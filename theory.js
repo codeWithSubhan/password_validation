038bce8d05f25c566e00c728cd1616f5ee0d1f79
subhan <suvhan13@gmail.com>
2023-09-25 22:48:42 +0530

    local to remote

---
+++ theory.js
@@ -0,0 +1,9 @@
+// Init command
+// use to create a new git repo means first make project on pc then upload on git repo
+
+// git init
+// git remote add origin url
+// git remote -v to verify remote
+// git branch  to check branch
+// git branch -m main  to rename branch
+// git push origin main
